97b636ae7ba7ab116106773386faf7e427ca1cac
Patricia García Martínez <martinez171214@gmail.com>
2021-12-14 17:35:06 +0000

    Develop HTML structure

---
+++ src/lib/todo-item.svelte
@@ -0,0 +1,13 @@
+<div>
+    <form action="" method="">
+        <input type="hidden" name="done" value="" />
+        <button aria-label="Mark done/not done">Dot/NotDone</button>
+    </form>
+    <form action="" method="">
+        <input type="text" />
+        <button aria-label="Save Todo">Save</button>
+    </form>
+    <form action="" method="">
+        <button aria-label="Delete Todo">Delete</button>
+    </form>
+</div>
